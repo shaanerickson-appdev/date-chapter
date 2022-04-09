ab207d6c077b513cdd7b3b3da0e50e3edf1273f9
Shaan <shaan.erickson@chicagobooth.edu>
2022-04-08 21:01:24 -0500

    grading

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,9 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2022, the calendar day is: 1, and the month is: 7."
+
+require "date"
+
+t = Date.today
+
+p "The year is: " + t.year.to_s + ", the calendar day is: " + t.day.to_s + ", and the month is: " + t.month.to_s + "."
--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,9 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+
+require "date"
+
+t = Date.parse("December 21, 1995")
+
+p "Ruby is " + (Date.today-Date.parse("December 21, 1995")).to_i.to_s + " days old!"
